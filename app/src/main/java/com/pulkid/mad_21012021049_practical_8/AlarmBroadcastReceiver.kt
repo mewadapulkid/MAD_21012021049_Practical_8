64932aeef015eef8bdb96e0c89d0849ced1a0861
Pulkid <pulkidmewada21@gnu.ac.in>
2023-10-28 19:43:43 +0530

    Initial commit

---
+++ app/src/main/java/com/pulkid/mad_21012021049_practical_8/AlarmBroadcastReceiver.kt
@@ -0,0 +1,22 @@
+package com.pulkid.mad_21012021049_practical_8
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+class AlarmBrodcastReceiver : BroadcastReceiver() {
+    companion object{
+        val ALARMKEY="key"
+        val ALARM_STOP="value"
+        val ALARM_START="start"
+    }
+    override fun onReceive(context: Context, intent: Intent) {
+        // This method is called when the BroadcastReceiver is receiving an Intent broadcast.
+        val data=intent.getStringExtra(ALARMKEY)
+        val intent_service=Intent(context,AlarmService::class.java)
+        if(data== ALARM_START){
+            context.startService(intent_service)
+        }
+        else if(data== ALARM_STOP){
+            context.stopService(intent_service)
+        }
+    }
+}
---
+++ app/src/main/java/com/pulkid/mad_21012021049_practical_8/AlarmService.kt
@@ -0,0 +1,25 @@
+package com.pulkid.mad_21012021049_practical_8
+
+
+import android.app.Service
+import android.content.Intent
+import android.media.MediaPlayer
+import android.os.IBinder
+class AlarmService : Service() {
+    lateinit var player:MediaPlayer
+    override fun onBind(intent: Intent): IBinder {
+        TODO("Return the communication channel to the service.")
+    }
+    override fun onDestroy() {
+        player.stop()
+        super.onDestroy()
+    }
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        if(intent!=null){
+            player=MediaPlayer.create(this,R.raw.alarm)
+            player.start()
+        }
+        return START_STICKY
+    }
+}
+
